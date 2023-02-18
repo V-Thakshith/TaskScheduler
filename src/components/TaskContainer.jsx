3caf47a633533241249a612579201c062a90cc76
Thakshith <thakshith@gmail.com>
2023-02-18 12:49:25 +0530

    version: 3.0?

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -8,9 +8,8 @@ import React,{useState} from "react";
 
 const TaskContainer=()=>{
     const navigate=useNavigate()
-    const {sortDate,setSortDate}=useState('')
-    const {tasks,deleteFromTasks,updateFromTasks}=useGlobleContext()
-
+    const {tasks,deleteFromTasks,updateFromTasks,getDates,dates,setDate}=useGlobleContext()
+    
     const updateFromTasksInComponent=(eventName)=>{
         navigate("/UpdateTask")
         updateFromTasks(eventName)
@@ -20,10 +19,13 @@ const TaskContainer=()=>{
         <div className="container">
                 <div className='box'>
                 <h1>Task container  {<CiAlarmOn/>}</h1>
-                <input type="date" value={sortDate} className="sort-date-input" required></input>
+                <select onChange={(e)=>{setDate(e.target.value)}} onClick={()=>{getDates()}} className="sort-date-input">
+                    <option value="default">Select a date</option>
+                    {dates.map((date)=><option className="sort-date-option" value={date}>{date}</option>)}
+                </select>
                 <div className="card-collection">
                 {(tasks!==[])? tasks.map((task)=>{return <><div className="card bg-danger"><div className="two-btn-pos"><div className="close-btn-pos" value={task.eventName} onClick={()=>{console.log(task.eventName); updateFromTasksInComponent(task.eventName)}}><CiEdit size={40}/></div>
-                <div className="close-btn-pos" value={task.eventName} onClick={()=>{console.log(task.eventName); deleteFromTasks(task.eventName)}}><IoMdClose size={40}/></div></div>
+                <div className="close-btn-pos" value={task.eventName} onClick={()=>{ deleteFromTasks(task.eventName)}}><IoMdClose size={40}/></div></div>
                                             <h1 className="card-title">{task.eventName}</h1>
                                             <h3 className="card-subtitle">{task.eventDate}</h3>
                                             <h4 className="card-text">{task.eventTime}</h4></div></>}):<h1 className="card-title">No Tasks😁😁</h1>}
--- src/context.jsx
+++ src/context.jsx
@@ -5,6 +5,35 @@ const AppContext=React.createContext();
 const AppProvider=({children})=>{
     const [tasks,setTasks]=useState(JSON.parse(localStorage.getItem('taskList')));
     const [task,setTask]=useState({})
+    const [dates,setDates]=useState([])
+    const [selectedDate,setSelectedDate]=useState();
+
+    const setDate=(date)=>{
+        let tasks=JSON.parse(localStorage.getItem('taskList'))
+        setSelectedDate(date);
+        console.log(date);
+        if(date==="default"){
+            console.log("hi")
+            setTasks(JSON.parse(localStorage.getItem('taskList')))
+        }
+        else{
+            setTasks(tasks.filter((task)=>task.eventDate===date))
+        }
+    }   
+
+    const getDates=()=>{
+        let tasks=JSON.parse(localStorage.getItem('taskList'))
+        let updatedDates=[]
+        for(let i=0;i<tasks.length;i++){
+            if(updatedDates.find((date)=>date===tasks[i].eventDate)){
+
+            }
+            else{
+                updatedDates=[...updatedDates,tasks[i].eventDate]
+            }
+        }
+        setDates(updatedDates)
+    }
 
     const deleteFromTasks=(eventName)=>{
         const updatedTasks=tasks.filter((task)=>task.eventName!==eventName)
@@ -28,7 +57,7 @@ const AppProvider=({children})=>{
         deleteFromTasks(task.eventName)
     }
 
-    return <AppContext.Provider value={{tasks,addToTasks,deleteFromTasks,updateFromTasks,task}}>
+    return <AppContext.Provider value={{tasks,addToTasks,deleteFromTasks,updateFromTasks,task,getDates,dates,setDate}}>
         {children}
     </AppContext.Provider>
 
